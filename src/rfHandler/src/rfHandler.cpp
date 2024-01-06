6a443d00d8b6613d33492331fefbe5e7d5e7dcee
MrNoname3 <26522650+MrNoname3@users.noreply.github.com>
2024-01-06 19:58:14 +0100

    Filter added to received RF data. It can filter out, the redundant received data in a short time.

--- src/rfHandler/src/rfHandler.cpp
+++ src/rfHandler/src/rfHandler.cpp
@@ -27,6 +27,8 @@ bool RfHandler::begin() { return true; }
 
 bool RfHandler::loop() {
   if(rfTransciever.available()) {                                         // Check if RF data received.
+    static RFData rfDataOld;                                              // Save old data.
+    static uint32_t dataCheckTimer = 0;                                   // Serial data send cooldown timer.
     RFData rfData;
     rfData.data = rfTransciever.getReceivedValue();                       // Get received data.
     rfData.bitLength = rfTransciever.getReceivedBitlength();              // Get received data bit length.
@@ -34,11 +36,22 @@ bool RfHandler::loop() {
     rfData.pulseLength = rfTransciever.getReceivedDelay();                // Get received data pulse length.
     rfTransciever.resetAvailable();                                       // Clear receive flag.
 
-    char dataOut[dataOutSize] = { '\0' };
-    const int32_t dataOutSize = snprintf_P(dataOut, sizeof(dataOut), RF_MSG_FRAME, MqttComBase::getIsoTime(), rfData.data, rfData.bitLength, rfData.protocol, rfData.pulseLength);
-    const bool dataOutValid = (dataOutSize >= 0 && dataOutSize < static_cast<int32_t>(sizeof(dataOut)));
-    if(!dataOutValid) { return false; }
-    MqttComBase::messageSend(dataOut);
+    if(millis() - dataCheckTimer >= 100) {                                // If timer is expired, clear old data to pass the next filter.
+      rfDataOld.data = 0;
+      rfDataOld.bitLength = 0;
+      rfDataOld.protocol = 0;
+    }
+
+    // Filter repeated data.
+    if((rfDataOld.data != rfData.data) || (rfDataOld.bitLength != rfData.bitLength) || (rfDataOld.protocol != rfData.protocol)) {
+      char dataOut[dataOutSize] = { '\0' };
+      const int32_t dataOutSize = snprintf_P(dataOut, sizeof(dataOut), RF_MSG_FRAME, MqttComBase::getIsoTime(), rfData.data, rfData.bitLength, rfData.protocol, rfData.pulseLength);
+      const bool dataOutValid = (dataOutSize >= 0 && dataOutSize < static_cast<int32_t>(sizeof(dataOut)));
+      if(!dataOutValid) { return false; }
+      MqttComBase::messageSend(dataOut);
+      rfDataOld = rfData;                                                 // Save sent data to use it for filtering.
+    }
+    dataCheckTimer = millis();                                            // Reload timer.
   }
   return true;
 }
