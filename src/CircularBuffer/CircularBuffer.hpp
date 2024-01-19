46aa9438c614000a26c6890f8edcf6a33111bbeb
MrNoname3 <26522650+MrNoname3@users.noreply.github.com>
2024-01-19 17:48:35 +0100

    Fix CircularBuffer by moving back implementation into header. (This is the good practice for templated classes.)

--- src/CircularBuffer/CircularBuffer.hpp
+++ src/CircularBuffer/CircularBuffer.hpp
@@ -52,4 +52,53 @@ private:
   uint16_t tail;                              // Circular buffer read index.
   bool full;                                  // Stores if the buffer full is.
 };
+
+template<class T, uint16_t buffSize>
+CircularBuffer<T, buffSize>::CircularBuffer() : buffer(), head(0), tail(0), full(false) {}
+
+template<class T, uint16_t buffSize>
+void CircularBuffer<T, buffSize>::put(T item) {
+  buffer[head] = item;
+  head = (head + 1) % buffSize;
+  if(full) { tail = (tail + 1) % buffSize; }
+  full = (head == tail);
+}
+
+template<class T, uint16_t buffSize>
+T CircularBuffer<T, buffSize>::pop() {
+  auto result = buffer[tail];
+  tail = (tail + 1) % buffSize;
+  full = false;
+  return result;
+}
+
+template<class T, uint16_t buffSize>
+void CircularBuffer<T, buffSize>::clear() {
+  head = 0;
+  tail = 0;
+  full = false;
+}
+
+template<class T, uint16_t buffSize>
+bool CircularBuffer<T, buffSize>::isEmpty() const {
+  if(full) { return false; }
+  return tail == head;
+}
+
+template<class T, uint16_t buffSize>
+bool CircularBuffer<T, buffSize>::isFull() const {
+  return full;
+}
+
+template<class T, uint16_t buffSize>
+uint16_t CircularBuffer<T, buffSize>::getCapacity() const {
+  return buffSize;
+}
+
+template<class T, uint16_t buffSize>
+uint16_t CircularBuffer<T, buffSize>::getSize() const {
+  if(full) { return buffSize; }
+  if(head >= tail) { return head - tail; }
+  return buffSize + head - tail;
+}
 #endif // CIRCULAR_BUFFER_HPP
