6e52cdd537d2741570de275391227ac4d43f054f
MrNoname3 <26522650+MrNoname3@users.noreply.github.com>
2024-02-21 19:41:53 +0100

    Added .bin to compilation outputs.

---
+++ elfToBin.py
@@ -0,0 +1,9 @@
+Import("env")
+# Custom BIN from ELF
+env.AddPostAction(
+    "$BUILD_DIR/${PROGNAME}.elf",
+    env.VerboseAction(" ".join([
+        "$OBJCOPY", "-O", "binary",
+        "$BUILD_DIR/${PROGNAME}.elf", "$BUILD_DIR/${PROGNAME}.bin"
+    ]), "Building $BUILD_DIR/${PROGNAME}.bin")
+)
