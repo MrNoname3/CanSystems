2758f7eff384af32f33d205709f95d781871c1be
MrNoname3 <26522650+MrNoname3@users.noreply.github.com>
2024-02-24 15:31:41 +0100

    Added CanHandlerClass, which handles the CAN communication and some basic functionality.

---
+++ src/canHandler/src/canHandler.cpp
@@ -0,0 +1,116 @@
+#include "canHandler.hpp"
+#include <CAN.h>                              /// CAN controller library.
+#include <avr/wdt.h>                          /// Watchdog timer library.
+
+CircularBuffer<CanHandler::CanFrame, CanHandler::rxBufferSize> CanHandler::rxBuffer;
+
+CanHandler::CanHandler(HardwareSerial& serial, uint8_t csPin, uint8_t intPin, uint8_t ledPin) :
+  serialPort(serial),
+  localCanId(0),
+  eepromHandler(&localCanId),
+  ledPin(ledPin)
+{
+  wdt_enable(WDTO_2S);                        // Enable WDT timer.
+  CAN.setPins(csPin, intPin);
+  pinMode(ledPin, OUTPUT);
+}
+
+bool CanHandler::begin(uint32_t canBaud) {
+  {
+    serialPort.print(F("Address: "));
+    const bool eepromDataValid = eepromHandler.load();
+    eepromDataValid ? serialPort.println(localCanId) : serialPort.println(ERR_STATE);
+    if(!eepromDataValid) { return false; }
+  }
+  {
+    serialPort.print(F("CAN: "));
+    CAN.setClockFrequency(8E6);                     // SPI CAN controller runs from 8MHz crystal.
+    const bool canBeginResult = CAN.begin(canBaud) == 1;
+    canBeginResult ? serialPort.println(OK_STATE) : serialPort.println(ERR_STATE);
+    if(!canBeginResult) { return false; }
+  }
+  { // Calculate the mask to ignore the upper bits of the extended CAN ID and only consider the lower 10 bits.
+    const uint16_t deviceAddress = localCanId;
+    const uint32_t mask = 0x7FF;                    // Mask for lower 10 bits (0b1111111111).
+    const uint32_t id = deviceAddress & mask;       // Calculate the ID using the device's local address.
+    serialPort.print(F("Filter: "));
+    const bool setFilterResult = CAN.filterExtended(id, mask) == 1;
+    setFilterResult ? serialPort.println(OK_STATE) : serialPort.println(ERR_STATE);
+    if(!setFilterResult) { return false; }
+    CAN.onReceive(rxInterrupt);
+  }
+  send(CanCmd::RESTART);
+  wdt_reset();                                                    // Reset the watchdog timer.
+  return true;
+}
+
+bool CanHandler::loop() {
+  static uint32_t pingTimer = millis();
+  if(!rxBuffer.isEmpty()) {
+    pingTimer = millis();                                         // Ping timer reload.
+    ledOff();
+    CanFrame canFrame = rxBuffer.pop();
+    switch(static_cast<uint16_t>(canFrame.cmd)) {
+      case static_cast<uint16_t>(CanCmd::PING): { send(CanCmd::PING); } break;
+      case static_cast<uint16_t>(CanCmd::RESTART): { restartMCU(); } break;
+    }
+  }
+  if(millis() - pingTimer >= pingTime) {                          // Check if ping timer is expired.
+    ledOn();                                                      // If yes, turn on the LED.
+  }
+  wdt_reset();                                                    // Reset the watchdog timer.
+  return true;
+}
+
+bool CanHandler::send(uint16_t command, const uint8_t (&data)[8]) const {
+  const uint32_t extId =
+    ((static_cast<uint32_t>(masterCanId) & 0x3FF) << 0) |
+    ((static_cast<uint32_t>(command) & 0x1FF) << 10) |
+    ((static_cast<uint32_t>(localCanId) & 0x3FF) << 19);
+  const bool beginPacketResult = CAN.beginExtendedPacket(extId) > 0;
+  if(!beginPacketResult) { return false; }
+  const bool packetWriteResult = CAN.write(data, sizeof(data)) > 0;
+  if(!packetWriteResult) { return false; }
+  const bool endPacketResult = CAN.endPacket() > 0;
+  if(!endPacketResult) { return false; }
+  return true;
+}
+
+bool CanHandler::send(CanCmd command, const uint8_t (&data)[8]) const {
+  return send(static_cast<uint16_t>(command), data);
+}
+
+bool CanHandler::send(uint16_t command) const {
+  uint8_t data[8] = { 0 };
+  return send(command, data);
+}
+
+bool CanHandler::send(CanCmd command) const {
+  return send(static_cast<uint16_t>(command));
+}
+
+void CanHandler::rxInterrupt(int packetsNum) {
+  if(packetsNum <= 0) { return; }
+  if(!CAN.packetExtended()) { return; }
+  CanFrame rxCanData;
+  rxCanData.extId = CAN.packetId();
+  if(!CAN.packetRtr()) {
+    const uint8_t canDataDlc = static_cast<uint8_t>(CAN.packetDlc());
+    const uint8_t bytesReaded = static_cast<uint8_t>(CAN.readBytes(rxCanData.data, canDataDlc));
+    if(canDataDlc != bytesReaded) { return; }
+  }
+  if(!rxBuffer.isFull()) { rxBuffer.put(rxCanData); }
+}
+
+void CanHandler::ledOn() { digitalWrite(ledPin, HIGH); }
+
+void CanHandler::ledOff() { digitalWrite(ledPin, LOW); }
+
+void CanHandler::ledToggle() { digitalWrite(ledPin, !digitalRead(ledPin)); }
+
+void CanHandler::restartMCU() {
+  serialPort.println(F("Restarting..."));
+  serialPort.flush();                                 // Sends out data from serial buffer, before reset.
+  wdt_enable(WDTO_15MS);                              // Setup watchdog timer.
+  while(true) { };                                    // Let the WDT restart the MCU.
+}
---
+++ src/canHandler/src/canHandler.hpp
@@ -0,0 +1,72 @@
+#ifndef CAN_HANDLER_HPP
+#define CAN_HANDLER_HPP
+
+#include <Arduino.h>                          /// Arduino libraries header.
+#include <HardwareSerial.h>
+#include "eepromHandler.hpp"                  /// EEPROM wrapper class.
+#include "../../CircularBuffer/src/CircularBuffer.hpp"  /// Circular buffer class.
+
+class CanHandler final {
+public:
+  static constexpr const char* OK_STATE               = "[OK]";   // OK status.
+  static constexpr const char* ERR_STATE              = "[ERR]";  // Error status.
+
+  /// @brief Base command list for nodes.
+  enum class CanCmd : uint16_t {
+    PING = 0,                                   // Ping command.
+    RESTART,                                    // Node reset command.
+    BUTTON_EVENT,                               // Get button event type.
+    FILET_START,                                // Init file transfer process.
+    FILET_SEND,                                 // Send file pieces.
+    FILET_END                                   // File transfer process ended.
+  };
+
+  struct __attribute__((packed))
+  CanFrame {                                    // CAN frame.
+    union {
+      uint32_t extId;                           // Extended CAN ID.
+      struct {
+        uint32_t to : 10;                       // 10 bits for receiver address.
+        uint32_t cmd : 9;                       // 9 bits for command.
+        uint32_t from : 10;                     // 10 bits for sender address.
+        uint32_t padding : 3;                   // Padding to fill up to 32 bits.
+      };
+    };
+    uint8_t data[8];                            // CAN data.
+    CanFrame() : extId(), data{0} {}
+  };
+
+  CanHandler(HardwareSerial& serial, uint8_t csPin, uint8_t intPin, uint8_t ledPin);
+  /// @brief Destructor of the object.
+  ~CanHandler() = default;
+  bool begin(uint32_t canBaud);
+  bool loop();
+  bool send(uint16_t command, const uint8_t (&data)[8]) const;
+  bool send(uint16_t command) const;
+  void ledOn();
+  void ledOff();
+  void ledToggle();
+  /// @brief Reset the MCU.
+  void restartMCU();
+
+  CanHandler(const CanHandler&) = delete;                       // Define copy constructor.
+  CanHandler& operator=(const CanHandler&) = delete;            // Define copy assignment operator.
+  CanHandler(CanHandler&&) = delete;                            // Define move constructor.
+  CanHandler& operator=(CanHandler&&) = delete;                 // Define move assignment operator.
+
+private:
+  bool send(CanCmd command, const uint8_t (&data)[8]) const;
+  bool send(CanCmd command) const;
+  static inline void rxInterrupt(int packetsNum) __attribute__((optimize("-O3")));
+
+  static constexpr uint16_t masterCanId = 10U;
+  static constexpr uint8_t rxBufferSize = 5;
+  static constexpr uint16_t pingTime = 1500;                    // Ping timeot time in ms.
+  HardwareSerial& serialPort;
+  uint16_t localCanId;
+  EEPROMHandler<uint16_t, 0> eepromHandler;
+  const uint8_t ledPin;
+  static CircularBuffer<CanFrame, rxBufferSize> rxBuffer;       // Ringbuffer of received CAN frames.
+};
+
+#endif // CAN_HANDLER_HPP
