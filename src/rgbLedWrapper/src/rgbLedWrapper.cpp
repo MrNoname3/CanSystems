9674367e506ea2ae89bfa64ddadee9389f4a0ee6
MrNoname3 <26522650+MrNoname3@users.noreply.github.com>
2024-02-25 16:40:23 +0100

    Added a wrapper class around RGB LED strip driver.

---
+++ src/rgbLedWrapper/src/rgbLedWrapper.cpp
@@ -0,0 +1,17 @@
+#include "rgbLedWrapper.hpp"
+
+RgbLedWrapper::RgbLedWrapper(const uint8_t ledNumber, const uint8_t ledPin) :
+  ledStrip(ledNumber, ledPin)
+{
+  ledStrip.Begin();               // Clear LEDs and show it.
+  ledStrip.Show();
+}
+
+void RgbLedWrapper::setColor(const uint8_t red, const uint8_t green, const uint8_t blue) {
+  ledStrip.ClearTo(RgbColor(red, green, blue));
+  ledStrip.Show();
+}
+
+void RgbLedWrapper::clear() {
+  setColor( 0U, 0U, 0U);
+}
---
+++ src/rgbLedWrapper/src/rgbLedWrapper.hpp
@@ -0,0 +1,26 @@
+#ifndef RGB_LED_WRAPPER_HPP
+#define RGB_LED_WRAPPER_HPP
+
+#include <stdint.h>
+#include <NeoPixelBus.h>                      /// WS2812 LED driver library.
+
+class RgbLedWrapper final {
+public:
+  RgbLedWrapper(const uint8_t ledNumber, const uint8_t ledPin);
+   /// @brief Destructor of the object.
+  ~RgbLedWrapper() = default;
+  /// @brief Send the given data to the RGB LED(s).
+  /// @param red Value of red color: 0-255.
+  /// @param green Value of green color: 0-255.
+  /// @param blue Value of blue color: 0-255.
+  void setColor(const uint8_t red, const uint8_t green, const uint8_t blue);
+  void clear();
+
+  RgbLedWrapper(const RgbLedWrapper&) = delete;                       // Define copy constructor.
+  RgbLedWrapper& operator=(const RgbLedWrapper&) = delete;            // Define copy assignment operator.
+  RgbLedWrapper(RgbLedWrapper&&) = delete;                            // Define move constructor.
+  RgbLedWrapper& operator=(RgbLedWrapper&&) = delete;                 // Define move assignment operator
+private:
+  NeoPixelBus<NeoGrbFeature, NeoWs2812xMethod> ledStrip;
+};
+#endif // RGB_LED_WRAPPER_HPP
