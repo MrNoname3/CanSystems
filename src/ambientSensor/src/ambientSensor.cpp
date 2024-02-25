68bc38c06e8b28671573d2d6c794e5e756670a0f
MrNoname3 <26522650+MrNoname3@users.noreply.github.com>
2024-02-25 13:57:25 +0100

    Added AmbientSensor class to read the temperature, humidity and light values from the sensors.

---
+++ src/ambientSensor/src/ambientSensor.cpp
@@ -0,0 +1,59 @@
+#include "ambientSensor.hpp"
+
+AmbientSensor::AmbientSensor(HardwareSerial& serial, CanHandler& canHandler, uint8_t lightPin, uint32_t measurePeriod) :
+  si7021(),
+  serialPort(serial),
+  canHandler(canHandler),
+  lightPin(lightPin),
+  measurePeriod(measurePeriod)
+{
+  analogReference(DEFAULT);                                                   // Setup analog reference to 5V.
+  bitSet(ADCSRA, ADPS2);                                                      // Fast ADC, set prescaler to 16.
+  bitSet(ADCSRA, ADPS1);
+  bitClear(ADCSRA, ADPS0);
+  Wire.setClock(400000);                                                      // Set I2C bus speed.
+  Wire.setWireTimeout(20000, true);                                           // Set I2C timeout to 20ms.
+}
+
+void AmbientSensor::begin() {
+  serialPort.print(F("SI7021: "));
+  const bool si7021BeginResult = si7021.begin();
+  si7021BeginResult ? serialPort.println(CanHandler::OK_STATE) : serialPort.println(CanHandler::ERR_STATE);
+  //if(!si7021BeginResult) {
+    //measurePeriod = 0;
+    //return;
+  //}
+  //si7021.setPrecision(0x81);                                                  // Set humtemp sensor reading resolution.
+}
+
+void AmbientSensor::loop() {
+  static uint8_t lightValue = 0;
+  { // Complementer filter calculation.
+    static constexpr uint8_t adcInputFilterAlpha = 10;                          // Complementer filter ALPHA value.
+    uint8_t lightRaw = analogRead(lightPin) >> 2;                               // Analog read and map from 10bit to 8bit.
+    lightValue = ((adcInputFilterAlpha * lightRaw) + (100U - adcInputFilterAlpha) * lightValue) / 100U;
+  }
+  if(!si7021.sensorExists()) { return; }
+  static uint32_t measureTimer = millis();
+  if(millis() - measureTimer >= measurePeriod) {
+    measureTimer = millis();
+    const int16_t temperature = si7021.getCelsiusHundredths();
+    const uint16_t humidity = si7021.getHumidityPercent();
+    const bool dataInvalid = Wire.getWireTimeoutFlag();
+    if(dataInvalid) {
+      Wire.clearWireTimeoutFlag();
+      return;
+    }
+    const uint8_t data[8] = {
+      static_cast<uint8_t>((temperature >> 0) & 0xFF),
+      static_cast<uint8_t>((temperature >> 8) & 0xFF),
+      static_cast<uint8_t>((humidity >> 0) & 0xFF),
+      static_cast<uint8_t>((humidity >> 8) & 0xFF),
+      lightValue,
+      0,
+      0,
+      0
+    };
+    //canHandler.send(, data);
+  }
+}
---
+++ src/ambientSensor/src/ambientSensor.hpp
@@ -0,0 +1,29 @@
+#ifndef AMBIENT_SENSOR_HPP
+#define AMBIENT_SENSOR_HPP
+
+#include <Arduino.h>                          /// Arduino libraries header.
+#include "canHandler/src/canHandler.hpp"
+#include <SI7021.h>                           /// Temperature and humidity sensor driver.
+#include <HardwareSerial.h>
+
+class AmbientSensor final {
+public:
+  AmbientSensor(HardwareSerial& serial, CanHandler& canHandler, uint8_t lightPin, uint32_t measurePeriod);
+  /// @brief Destructor of the object.
+  ~AmbientSensor() = default;
+  void begin();
+  void loop();
+
+  AmbientSensor(const AmbientSensor&) = delete;                       // Define copy constructor.
+  AmbientSensor& operator=(const AmbientSensor&) = delete;            // Define copy assignment operator.
+  AmbientSensor(AmbientSensor&&) = delete;                            // Define move constructor.
+  AmbientSensor& operator=(AmbientSensor&&) = delete;                 // Define move assignment operator
+private:
+  SI7021 si7021;                                                      // I2C humidity and temperature sensor driver.
+  HardwareSerial& serialPort;
+  CanHandler& canHandler;
+  const uint8_t lightPin;
+  const uint32_t measurePeriod;
+};
+
+#endif // AMBIENT_SENSOR_HPP
