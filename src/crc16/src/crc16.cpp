ed407e862a591bbeceeb46edba8d05bd14dfe5d2
MrNoname3 <26522650+MrNoname3@users.noreply.github.com>
2024-02-25 14:19:13 +0100

    Added CRC16 calculator class.

---
+++ src/crc16/src/crc16.cpp
@@ -0,0 +1,32 @@
+#include "crc16.hpp"
+
+Crc16::Crc16(uint16_t initValue, uint16_t polynomial) :
+  crc_(initValue),
+  polynomial_(polynomial)
+{}
+
+void Crc16::next(uint8_t value) {
+  crc_ ^= ((uint16_t)value << 8);
+  for (int i = 0; i < 8; i++) {
+    if (crc_ & 0x8000) {
+      crc_ = (crc_ << 1) ^ polynomial_;
+    }
+    else {
+      crc_ <<= 1;
+    }
+  }
+}
+
+void Crc16::next(const uint8_t* values, uint32_t length) {
+  for (uint32_t i = 0; i < length; i++) {
+    next(values[i]);
+  }
+}
+
+uint16_t Crc16::get() const { return crc_; }
+
+uint16_t Crc16::calculate(const uint8_t *data, uint32_t length) {
+  Crc16 crc;
+  crc.next(data, length);
+  return crc.get();
+}
---
+++ src/crc16/src/crc16.hpp
@@ -0,0 +1,23 @@
+#ifndef CRC16_HPP
+#define CRC16_HPP
+
+#include <stdint.h>
+
+class Crc16 final {
+  public:
+    explicit Crc16(uint16_t initValue = 0xFFFF, uint16_t polynomial = 0x1021);
+    ~Crc16() = default;
+    void next(uint8_t value);
+    void next(const uint8_t* values, uint32_t length);
+    uint16_t get() const;
+    static uint16_t calculate(const uint8_t *data, uint32_t length);
+
+    Crc16(const Crc16&) = delete;                       // Define copy constructor.
+    Crc16& operator=(const Crc16&) = delete;            // Define copy assignment operator.
+    Crc16(Crc16&&) = delete;                            // Define move constructor.
+    Crc16& operator=(Crc16&&) = delete;                 // Define move assignment operator.
+  private:
+    uint16_t crc_;                                      // CRC16 starting value.
+    const uint16_t polynomial_;                         // CRC16 polynomial.
+  };
+#endif // CRC16_HPP
