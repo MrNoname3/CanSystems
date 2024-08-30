d7e65ef84ae56dfa5748e76ca14bf3529bc81480
MrNoname3 <26522650+MrNoname3@users.noreply.github.com>
2024-08-30 11:05:51 +0200

    Clean main.cpp for base environments.

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,7 @@
-//--- Headers ---//
-#include <Arduino.h>                          /// Arduino libraries header.
-
-//--- Variables ---//
-const char separator[] PROGMEM = "******************************************************";
+#include <Arduino.h>
 
 void setup() {
-  Serial.printf_P(PSTR("%s\r\nStarting...\r\n"), separator);
 
-  Serial.printf_P(PSTR("%s\r\nLoop starting...\r\n"), separator);
 }
 
 void loop() {
