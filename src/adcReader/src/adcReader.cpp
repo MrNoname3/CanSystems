fd8e8ffcbaf57a797690a921ef3b70bae483853f
MrNoname3 <26522650+MrNoname3@users.noreply.github.com>
2024-08-31 18:28:03 +0200

    Added AdcReader class for ADS1115.

---
+++ src/adcReader/src/adcReader.cpp
@@ -0,0 +1,115 @@
+#include "adcReader.hpp"
+
+volatile bool AdcReader::adcReady = false;
+const char AdcReader::MQTT_MSG_FRAME[] PROGMEM = {
+  "{"
+    "\"Time\":\"%s\","
+    "\"Analog\":[%hd,%hd,%hd,%hd],"
+    "\"Voltage\":[%.2f,%.2f,%.2f,%.2f]"
+  "}"
+};
+
+AdcReader::AdcReader(Connectivity& connectivity, const char* classID, uint16_t measureTime, uint8_t rdyPin, uint8_t address) :
+  MqttComBase(connectivity, classID),
+  ADS(address),
+  measureTime(measureTime),
+  rdyPin(rdyPin),
+  channel(0),
+  adcValues{0},
+  measureState(MeasureStates::IDLE),
+  measureTimer(0),
+  enableSending(false),
+  mqttSendTime(0),
+  mqttSendTimer(0)
+{
+  pinMode(rdyPin, INPUT_PULLUP);
+  Wire.begin();
+}
+
+bool AdcReader::begin() {
+  const bool initAdc =  ADS.begin();
+  if(initAdc) {
+    // Set ALERT/RDY pin.
+    ADS.setComparatorThresholdHigh(0x8000);
+    ADS.setComparatorThresholdLow(0x0000);
+    ADS.setComparatorQueConvert(0);
+    ADS.setComparatorPolarity(1);
+    attachInterrupt(digitalPinToInterrupt(rdyPin), intHandler, FALLING);
+    measureState = MeasureStates::REQUEST_ADC;
+  }
+  adcReady = false;
+  mqttSendTimer = millis();
+  const bool adsHasError = ADS.getError() != ADS1X15_OK;
+  return initAdc && !adsHasError;
+}
+
+void AdcReader::end() {
+  detachInterrupt(digitalPinToInterrupt(rdyPin));
+  measureState = MeasureStates::IDLE;
+}
+
+bool AdcReader::loop() {
+  switch(measureState) {
+    case MeasureStates::IDLE: {
+      if(adcReady) {
+        adcReady = false;
+        measureState = MeasureStates::STORE_DATA;
+      }
+    } break;
+    case MeasureStates::REQUEST_ADC: {
+      ADS.requestADC(channel);
+      measureState = MeasureStates::IDLE;
+    } break;
+    case MeasureStates::STORE_DATA: {
+      if(ADS.isReady()) {
+        adcValues[channel] = ADS.getValue();
+        measureTimer = millis();
+        measureState = MeasureStates::MEASURE_DELAY;
+      }
+    } break;
+    case MeasureStates::MEASURE_DELAY: {
+      if((millis() - measureTimer) >= measureTime) {
+        if((channel == maxChannelNumber) && enableSending) {
+          if(millis() - mqttSendTimer >= mqttSendTime) {
+            mqttSendTimer = millis();
+            //Serial.printf_P(PSTR("ADC: %hd | %hd | %hd | %hd\r\n"), adcValues[0], adcValues[1], adcValues[2], adcValues[3]);
+            char dataOut[dataOutBufSize] = { '\0' };
+            const int32_t dataOutSize = snprintf_P(dataOut, sizeof(dataOut), MQTT_MSG_FRAME, MqttComBase::getIsoTime(),
+              adcValues[0], adcValues[1], adcValues[2], adcValues[3],
+              ADS.toVoltage(adcValues[0]), ADS.toVoltage(adcValues[1]), ADS.toVoltage(adcValues[2]), ADS.toVoltage(adcValues[3]));
+            const bool dataOutValid = (dataOutSize >= 0 && dataOutSize < static_cast<int32_t>(sizeof(dataOut)));
+            if(!dataOutValid) { return false; }
+            MqttComBase::messageSend(dataOut);
+          }
+        }
+        channel = (channel + 1) & maxChannelNumber;
+        measureState = MeasureStates::REQUEST_ADC;
+      }
+    } break;
+  }
+  return ADS.getError() == ADS1X15_OK ? true : false;
+}
+
+void AdcReader::messageReceived(uint8_t* payload, uint32_t length) {}
+
+int16_t AdcReader::analogRead(Channel channel) {
+  return adcValues[static_cast<uint8_t>(channel) & maxChannelNumber];
+}
+
+ float AdcReader::voltageRead(Channel channel) {
+  return ADS.toVoltage(adcValues[static_cast<uint8_t>(channel) & maxChannelNumber]);
+ }
+
+void AdcReader::enableMqttSending(uint32_t interval) {
+  const uint32_t minAllowedInterval = measureTime * analogChannels;
+  mqttSendTime = interval > minAllowedInterval ? interval : minAllowedInterval;
+  enableSending = true;
+}
+
+void AdcReader::disableMqttSending() {
+  enableSending = false;
+}
+
+void AdcReader::intHandler() {
+  adcReady = true;
+}
--- src/main_esp8266_smoke.cpp
+++ src/main_esp8266_smoke.cpp
@@ -1,10 +1,12 @@
 //--- Headers ---//
 #include <Arduino.h>                          /// Arduino libraries header.
 #include "connectivity/src/connectivity.hpp"
+#include "adcReader/src/adcReader.hpp"
 
 //--- Constants ---//
 static constexpr const uint8_t LED                    = D8;           // Status LED.
 static constexpr const uint8_t SPI_CS                 = D0;           // Ethernet shield SPI CS.
+static constexpr const uint8_t ADC_RDY                = D3;           // ADC ready signal.
 
 //--- Variables ---//
 const char separator[] PROGMEM = "******************************************************";
@@ -13,10 +15,12 @@ const char separator[] PROGMEM = "**********************************************
 Connectivity iotConn(Serial, SPI_CS, LED, false);
 
 //--- MQTT handler objects ---//
+AdcReader adcReader(iotConn, "adcreader", 50U, ADC_RDY);
 
 void setup() {
   Serial.printf_P(PSTR("%s\r\nStarting...\r\n"), separator);
   iotConn.begin(Connectivity::Interface::WIFI, true);
+  adcReader.enableMqttSending(10000U);
   Serial.printf_P(PSTR("%s\r\nLoop starting...\r\n"), separator);
 }
 
