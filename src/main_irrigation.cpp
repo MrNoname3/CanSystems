a73a0f318eebc4ec701347c020b43163caf45172
MrNoname3 <26522650+MrNoname3@users.noreply.github.com>
2024-10-07 20:46:34 +0200

    Update main_irrigation.

--- src/main_irrigation.cpp
+++ src/main_irrigation.cpp
@@ -11,8 +11,13 @@ static constexpr uint8_t LED_PIN                    = 4U;           // Pin of th
 static constexpr uint8_t CAN_CS                     = 10U;          // CS pin of the SPI CAN controller.
 static constexpr uint8_t CAN_INT                    = 2U;           // Interrupt pin of the SPI CAN controller.
 static constexpr uint8_t FLASH_CS                   = 8U;           // SPI FLASH CS pin.
-static constexpr uint8_t BUTTON_PIN                 = 8U;           // Pushbutton pin.
-static constexpr uint8_t PUMP_0                     = 9U;
+static constexpr uint8_t BUTTON_PIN                 = 6U;           // Pushbutton pin.
+static constexpr uint8_t FLOW_INT                   = 3U;           // Interrupt pin of the flow sensor.
+static constexpr uint8_t PUMP_PWM                   = 5U;           // PWM pin of the pump.
+static constexpr uint8_t ANALOG_EN                  = 9U;           // Analog power and multiplexer IC enable pin.
+static constexpr uint8_t ANALOG_CHS[4]        = {A0, A2, A3, A4};   // Analog multiplexer channel select pins.
+static constexpr uint8_t MOISTURE_SENSOR            = A6;           // Analog pin for moisture sensor.
+static constexpr uint8_t CURRENT_SENSOR             = A7;           // Analog pin for current sensor.
 
 //--- Functions ---//
 void canMessageArrived(uint16_t command, const uint8_t (&data)[8]);
@@ -21,14 +26,8 @@ void measureMaxLoopTime();
 
 //--- Driver objects ---//
 CanHandler canHandler(Serial, CAN_CS, CAN_INT, LED_PIN, FLASH_CS);
-PushButtonHandler buttonHandler(Serial, canHandler, BUTTON_PIN);
+PushButtonHandler buttonHandler(Serial, canHandler, [](){return static_cast<bool>(digitalRead(BUTTON_PIN));});
 RgbLedWrapper rgbLed(RGB_LED_NUM, RGB_PIN);
-
-//--- Array of function pointers ---//
-void (*methodCallers[])() = {
-  []() { buttonHandler.loop(); }
-};
-static constexpr uint8_t numMethods = sizeof(methodCallers) / sizeof(*methodCallers);
 
 //--- Setup section ---//
 void setup() {
@@ -38,94 +37,17 @@ void setup() {
   delay(1U);
   Serial.println(F("\r\n********\r\nStarting..."));
   canHandler.begin(500E3);                                                    // Set CAN speed to 500Kb/s.
+  pinMode(BUTTON_PIN, INPUT_PULLUP);                                          // Set button pin as input with pullup resistor.
   buttonHandler.addBtnCallback(btnEventHandling);
   rgbLed.begin();
   Serial.println(F("********\r\nLooping..."));
   canHandler.ledOff();
-  pinMode(PUMP_0, OUTPUT);
 }
-
-enum class IrrigationState : uint8_t {
-    IDLE = 0,
-    SPINUP,
-    RUNNING,
-    SPINDOWN,
-    STOPPED,
-    ERROR
-};
-IrrigationState irrigationState = IrrigationState::IDLE;
 
 void loop() {
   canHandler.loop();
-  static uint8_t methodIndex = 0U;
-  methodCallers[methodIndex++]();
-  if(methodIndex >= numMethods) { methodIndex = 0U; }
+  buttonHandler.loop();
   //measureMaxLoopTime();
-  //const uint8_t pwmValue = map(analogRead(A0), 0U, 1023U, 0, 255U);
-  //analogWrite(PUMP_0, pwmValue);
-  //Serial.println(pwmValue);
-  static bool irrigation = true;
-  static uint32_t spinUpTimer = 0U;
-  const uint32_t spinUpTime = 100U;
-  static uint8_t pwmValue = 0U;
-  static uint32_t runningTimer = 0U;
-  const uint32_t runningTime = 40000U;
-  static uint32_t spinDownTimer = 0U;
-  const uint32_t spinDownTime = 300U;
-  static uint32_t irrigationTimer = 0U;
-  const uint32_t irrigationTime = 1800000U;
-
-  if(millis() - irrigationTimer > irrigationTime) {
-    irrigation = true;
-    irrigationTimer = millis();
-  }
-
-  switch(irrigationState) {
-    case IrrigationState::IDLE: {
-      if(irrigation == true) {
-        Serial.println("Irrigation");
-        //pwmValue = 190U;
-        pwmValue = 255U;
-        spinUpTimer = millis();
-        irrigationState = IrrigationState::SPINUP;
-      }
-    } break;
-    case IrrigationState::SPINUP: {
-      if(millis() - spinUpTimer >= spinUpTime) {
-        pwmValue -= 10U;
-        spinUpTimer = millis();
-        if(pwmValue <= 150U) {
-          runningTimer = millis();
-          irrigationState = IrrigationState::RUNNING;
-        }
-      }
-    } break;
-    case IrrigationState::RUNNING: {
-      if(millis() - runningTimer >= runningTime) {
-        spinDownTimer = millis();
-        irrigationState = IrrigationState::SPINDOWN;
-      }
-    } break;
-    case IrrigationState::SPINDOWN: {
-      if(millis() - spinDownTimer >= spinDownTime)  {
-        spinDownTimer = millis();
-        pwmValue -= 5U;
-        if(pwmValue <= 50U) {
-          irrigationState = IrrigationState::STOPPED;
-        }
-      }
-
-    } break;
-    case IrrigationState::STOPPED:
-    case IrrigationState::ERROR: {
-      pwmValue = 0U;
-      irrigationState = IrrigationState::IDLE;
-      irrigation = false;
-    } break;
-  }
-  analogWrite(PUMP_0, pwmValue);
-  
-
 }
 
 void canMessageArrived(uint16_t command, const uint8_t (&data)[8]) {
@@ -134,11 +56,6 @@ void canMessageArrived(uint16_t command, const uint8_t (&data)[8]) {
 
 void btnEventHandling(PushButtonHandler::BtnEvent btnEvent) {
   switch(btnEvent) {
-    case PushButtonHandler::BtnEvent::LONG_PRESS: {
-      //static bool rgbLedState = false;
-      //rgbLedState = !rgbLedState;
-      //rgbLedState ? rgbLed.setColor(50U, 50U, 50U, true) : rgbLed.clear();
-    } break;
     default: {} break;
   }
 }
