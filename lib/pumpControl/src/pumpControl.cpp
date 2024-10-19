e1b31eeba7fdb984aad152face5d5eff72478e0b
MrNoname3 <26522650+MrNoname3@users.noreply.github.com>
2024-10-19 19:33:28 +0200

    Added irrigation queue to PumpControl class.

--- lib/pumpControl/src/pumpControl.cpp
+++ lib/pumpControl/src/pumpControl.cpp
@@ -21,13 +21,25 @@ void PumpControl::run() {
 
 }
 
+void PumpControl::createIrrigation(uint8_t irrigationInfo, uint8_t pwmValue, uint8_t repeatNum) {
+  if(!irrigationQueue.isFull()) {
+    irrigationQueue.put(IrrigationQueueElement(irrigationInfo, pwmValue, repeatNum));
+  }
+}
+
+void PumpControl::createIrrigation(uint8_t channel, uint8_t duration, uint8_t pwmValue, uint8_t repeatNum, bool checkFlow) {
+  if(!irrigationQueue.isFull()) {
+    irrigationQueue.put(IrrigationQueueElement(channel, duration, checkFlow, pwmValue, repeatNum));
+  }
+}
+
 void PumpControl::irqHandler() {
   flowCounter++;
 }
 
-bool PumpControl::selectChannel(CH channel) {
+bool PumpControl::selectChannel(uint8_t channel) const {
   const uint8_t actualRegValue = pcf.getRegisterValue();
   uint8_t newRegValue = actualRegValue & 0xF0;            // Keep high 4 bits, clear low 4 bits.
-  newRegValue |= static_cast<uint8_t>(channel);           // Apply the new channel selection.
+  newRegValue |= (1U << channel);                         // Apply the new channel selection.
   return pcf.write(newRegValue);
 }
--- lib/pumpControl/src/pumpControl.hpp
+++ lib/pumpControl/src/pumpControl.hpp
@@ -4,6 +4,7 @@
 #include <stdint.h>
 #include "taskRunner.hpp"
 #include "pcf8574.hpp"
+#include "CircularBuffer.hpp"                                       /// Circular buffer class.
 
 class PumpControl final : public TaskRunner {
 public:
@@ -12,25 +13,41 @@ public:
 
   virtual void init() override;
   virtual void run() override;
+  void createIrrigation(uint8_t irrigationInfo, uint8_t pwmValue, uint8_t repeatNum);
+  void createIrrigation(uint8_t channel, uint8_t duration, uint8_t pwmValue, uint8_t repeatNum, bool checkFlow);
 
   PumpControl(const PumpControl&) = delete;               // Define copy constructor.
   PumpControl& operator=(const PumpControl&) = delete;    // Define copy assignment operator.
   PumpControl(PumpControl&&) = delete;                    // Define move constructor.
   PumpControl& operator=(PumpControl&&) = delete;         // Define move assignment operator.
 private:
-  enum class CH : uint8_t {
-    CHANNEL1 = 1U << 0U,
-    CHANNEL2 = 1U << 1U,
-    CHANNEL3 = 1U << 2U,
-    CHANNEL4 = 1U << 3U
+  struct __attribute__((packed))
+  IrrigationQueueElement {
+    union {
+      uint8_t irrigationInfo;
+      struct {
+        uint8_t channel : 2;
+        uint8_t duration : 3;
+        uint8_t checkFlow : 1;
+        uint8_t padding : 2;
+      };
+    };
+    uint8_t pwmValue;
+    uint8_t repeatNum;
+    IrrigationQueueElement() : irrigationInfo(0U), pwmValue(0U), repeatNum(0U) {}
+    IrrigationQueueElement(uint8_t irrigatinInfo, uint8_t pwmValue, uint8_t repeatNum) :
+      irrigationInfo(irrigatinInfo), pwmValue(pwmValue), repeatNum(repeatNum) {}
+    IrrigationQueueElement(uint8_t channel, uint8_t duration, bool checkFlow, uint8_t pwmValue, uint8_t repeatNum) :
+      channel(channel &= 3U), duration(duration & 7U), checkFlow(static_cast<uint8_t>(checkFlow)), pwmValue(pwmValue), repeatNum(repeatNum) {}
   };
 
   static void irqHandler();
-  bool selectChannel(CH channel);
+  bool selectChannel(uint8_t channel) const;
 
   PCF8574& pcf;
   const uint8_t pwmPin;
   const uint8_t intPin;
   static volatile uint16_t flowCounter;
+  CircularBuffer<IrrigationQueueElement, 4> irrigationQueue;
 };
 #endif //PUMP_CONTROL_HPP
