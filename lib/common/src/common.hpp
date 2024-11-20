afa69a67598d8abf865077268631df9ec45f26b3
MrNoname3 <26522650+MrNoname3@users.noreply.github.com>
2024-11-20 20:01:31 +0100

    Added documentation to "TimeConverter" class.

--- lib/common/src/common.hpp
+++ lib/common/src/common.hpp
@@ -1,14 +1,39 @@
 #ifndef COMMON_HPP
 #define COMMON_HPP
 
-#include <stdint.h>
+#include <stdint.h>                                                 /// Standard fixed-width integer types.
 
+/// @brief Utility class for converting time units into milliseconds or other units.
+/// @details Provides a collection of `constexpr` static methods to perform
+/// time conversions at compile-time or runtime.
 class TimeConverter {
 public:
-  static constexpr uint32_t hrToMs(uint16_t hour) { return hour * 60UL * 60UL * 1000UL; }
-  static constexpr uint32_t minToMs(uint16_t minute) { return minute * 60UL * 1000UL; }
-  static constexpr uint32_t secToMs(uint16_t second) { return second * 1000UL; }
-  static constexpr uint16_t hrToMin(uint16_t hour) { return hour * 60U; }
-};
+  /// @brief Converts hours to milliseconds.
+  /// @param hour The number of hours to convert.
+  /// @return The equivalent time in milliseconds.
+  static constexpr uint32_t hrToMs(uint16_t hour) {
+    return hour * 60UL * 60UL * 1000UL;
+  }
 
+  /// @brief Converts minutes to milliseconds.
+  /// @param minute The number of minutes to convert.
+  /// @return The equivalent time in milliseconds.
+  static constexpr uint32_t minToMs(uint16_t minute) {
+    return minute * 60UL * 1000UL;
+  }
+
+  /// @brief Converts seconds to milliseconds.
+  /// @param second The number of seconds to convert.
+  /// @return The equivalent time in milliseconds.
+  static constexpr uint32_t secToMs(uint16_t second) {
+    return second * 1000UL;
+  }
+
+  /// @brief Converts hours to minutes.
+  /// @param hour The number of hours to convert.
+  /// @return The equivalent time in minutes.
+  static constexpr uint16_t hrToMin(uint16_t hour) {
+    return hour * 60U;
+  }
+};
 #endif // COMMON_HPP
