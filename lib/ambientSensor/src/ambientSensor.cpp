a5e03224ace743e6045bd47ab37ea973d8d5e0ef
MrNoname3 <26522650+MrNoname3@users.noreply.github.com>
2024-11-29 18:45:52 +0100

    Improve "AmbientSensor" running time, by separate sensor interactions, and lower reading precision.

--- lib/ambientSensor/src/ambientSensor.cpp
+++ lib/ambientSensor/src/ambientSensor.cpp
@@ -8,15 +8,21 @@ AmbientSensor::AmbientSensor(CanHandler& canHandler, uint8_t lightPin, uint32_t 
   lightPin(lightPin),
   measurePeriod(measurePeriod),
   lightValue(0U),
-  eventTimer(0U)
+  temperature(0),
+  humidity(0U),
+  eventTimer(0U),
+  event(Event::IDLE)
 {
   Wire.setClock(clockSpeed);                        // Set I2C bus speed.
-  Wire.setWireTimeout(Time::msToUs(5U), true);      // Set I2C timeout to 20ms.
+  Wire.setWireTimeout(Time::msToUs(20U), true);     // Set I2C timeout to 20ms.
 }
 
 bool AmbientSensor::init() {
   const bool si7021BeginResult = si7021.begin();
-  if(si7021BeginResult) { si7021.setHeater(false); }
+  if(si7021BeginResult) {
+    si7021.setHeater(false);
+    si7021.setPrecision(0x81);
+  }
   eventTimer = millis();
   return si7021BeginResult;
 }
@@ -25,26 +31,40 @@ void AmbientSensor::run() {
   const uint32_t actualTime = millis();
   lightValue = Analog::complementaryFilter10(static_cast<uint16_t>(analogRead(lightPin)), lightValue);
   if(!si7021.sensorExists()) { return; }
-  if(Time::hasElapsed(actualTime, eventTimer, measurePeriod)) {
-    eventTimer = actualTime;
-    const int16_t temperature = si7021.getCelsiusHundredths();
-    const uint16_t humidity = si7021.getHumidityPercent();
-    const bool dataInvalid = Wire.getWireTimeoutFlag();
-    if(dataInvalid) {
-      Wire.clearWireTimeoutFlag();
-      canHandler.send(CanCmd::HUM_TEMP_I2C_TIMEOUT);
-      return;
-    }
-    const uint8_t data[8] = {
-      static_cast<uint8_t>((temperature >> 0U) & 0xFF),
-      static_cast<uint8_t>((temperature >> 8U) & 0xFF),
-      static_cast<uint8_t>((humidity >> 0U) & 0xFF),
-      static_cast<uint8_t>((humidity >> 8U) & 0xFF),
-      static_cast<uint8_t>((lightValue >> 0U) & 0xFF),
-      static_cast<uint8_t>((lightValue >> 8U) & 0xFF),
-      0U,
-      0U
-    };
-    canHandler.send(CanCmd::READ_HUM_TEMP_LDR, data);
-  }
+  switch(event) {
+    case Event::IDLE: {
+      if(Time::hasElapsed(actualTime, eventTimer, measurePeriod)) {
+        eventTimer = actualTime;
+        event = Event::READ_TEMPERATURE;
+      };
+    } break;
+    case Event::READ_TEMPERATURE: {
+      temperature = si7021.getCelsiusHundredths();
+      event = Event::READ_HUMIDITY;
+    } break;
+    case Event::READ_HUMIDITY: {
+      humidity = si7021.getHumidityPercent();
+      event = Event::SEND_VALUES;
+    } break;
+    case Event::SEND_VALUES: {
+      event = Event::IDLE;
+      const bool dataInvalid = Wire.getWireTimeoutFlag();
+      if(dataInvalid) {
+        Wire.clearWireTimeoutFlag();
+        canHandler.send(CanCmd::HUM_TEMP_I2C_TIMEOUT);
+        return;
+      }
+      const uint8_t data[8] = {
+        static_cast<uint8_t>((temperature >> 0U) & 0xFF),
+        static_cast<uint8_t>((temperature >> 8U) & 0xFF),
+        static_cast<uint8_t>((humidity >> 0U) & 0xFF),
+        static_cast<uint8_t>((humidity >> 8U) & 0xFF),
+        static_cast<uint8_t>((lightValue >> 0U) & 0xFF),
+        static_cast<uint8_t>((lightValue >> 8U) & 0xFF),
+        0U,
+        0U
+      };
+      canHandler.send(CanCmd::READ_HUM_TEMP_LDR, data);
+    } break;
+  };
 }
--- lib/ambientSensor/src/ambientSensor.hpp
+++ lib/ambientSensor/src/ambientSensor.hpp
@@ -34,6 +34,14 @@ public:
   AmbientSensor& operator=(AmbientSensor&&) = delete;                 // Define move assignment operator.
 
 private:
+  /// @brief Defines the state of the sensor task.
+  enum class Event : uint8_t {
+    IDLE = 0U,                        // Idle state, waiting for the next action.
+    READ_TEMPERATURE,                 // Read temperature from the SI7021 sensor.
+    READ_HUMIDITY,                    // Read humidity from the SI7021 sensor.
+    SEND_VALUES                       // Transmit the collected data.
+  };
+
   static constexpr uint32_t clockSpeed = 100000U;                           // Default I2C clock speed in Hz.
 
   SI7021 si7021;                                                            // I2C humidity and temperature sensor driver.
@@ -41,6 +49,9 @@ private:
   const uint8_t lightPin;                                                   // Analog pin for light intensity readings.
   const uint32_t measurePeriod;                                             // Measurement interval in milliseconds.
   uint16_t lightValue;                                                      // Filtered light intensity value.
+  int16_t temperature;                                                      // Current temperature reading in hundredths of a degree Celsius.
+  uint16_t humidity;                                                        // Current relative humidity reading in percentage.
   uint32_t eventTimer;                                                      // Class wide variable for universal timings.
+  Event event;                                                              // Current state of the sensor task.
 };
 #endif // AMBIENT_SENSOR_HPP
