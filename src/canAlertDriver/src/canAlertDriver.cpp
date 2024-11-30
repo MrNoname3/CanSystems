69b91ac0138727a71a22a7e9d4450d074425be13
MrNoname3 <26522650+MrNoname3@users.noreply.github.com>
2024-11-30 15:47:28 +0100

    Compatibility changes for light value sending in "CanAlertDriver".

--- src/canAlertDriver/src/canAlertDriver.cpp
+++ src/canAlertDriver/src/canAlertDriver.cpp
@@ -22,9 +22,10 @@ void CanAlertDriver::canFrameReceived(CanHandler::CanFrame& canFrame) {
   const uint16_t command = canFrame.cmd;
   switch(command) {
     case static_cast<uint16_t>(CanCmd::READ_HUM_TEMP_LDR): {
-      const float temperature = static_cast<float>((static_cast<uint16_t>(canFrame.data[0]) << 0) | (static_cast<uint16_t>(canFrame.data[1]) << 8)) / 100.0F + tempOffset;
-      const uint16_t humidity = (static_cast<uint16_t>(canFrame.data[2]) << 0) | (static_cast<uint16_t>(canFrame.data[3]) << 8);
-      const uint8_t light = canFrame.data[4];
+      const float temperature = static_cast<float>((static_cast<uint16_t>(canFrame.data[0]) << 0U) | (static_cast<uint16_t>(canFrame.data[1]) << 8U)) / 100.0F + tempOffset;
+      const uint16_t humidity = (static_cast<uint16_t>(canFrame.data[2]) << 0U) | (static_cast<uint16_t>(canFrame.data[3]) << 8U);
+      const uint16_t lightRaw = (static_cast<uint16_t>(canFrame.data[4]) << 0U) | (static_cast<uint16_t>(canFrame.data[5]) << 8U);
+      const uint8_t light = static_cast<uint8_t>(lightRaw >> 2U); // TODO: send the full 2byte to the server.
       char dataOut[dataOutBufSize] = { '\0' };
       const int32_t dataOutSize = snprintf_P(dataOut, sizeof(dataOut), HUM_TEMP_LDR_FRAME, MqttComBase::getIsoTime(), temperature, humidity, light);
       const bool dataOutValid = (dataOutSize >= 0 && dataOutSize < static_cast<int32_t>(sizeof(dataOut)));
