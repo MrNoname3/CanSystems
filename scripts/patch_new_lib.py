b7d1e40dbd4d9ede39b6eb20b398c51b54429034
MrNoname3 <26522650+MrNoname3@users.noreply.github.com>
2024-12-14 14:39:54 +0100

    Bump C++ version from 11 to 14 for ATmega328 projects. A new pre-script was added to patch the Arduino framework's new.h and new.cpp files.

---
+++ scripts/patch_new_lib.py
@@ -0,0 +1,28 @@
+import os
+import shutil
+
+Import("env")
+
+def patch_new_lib():
+  # Get the framework directory path
+  framework_dir = env.PioPlatform().get_package_dir("framework-arduino-avr")
+
+  if framework_dir:
+    # Define the path to the framework's original new.cpp and new.h
+    new_cpp_path = os.path.join(framework_dir, "cores", "arduino", "new.cpp")
+    new_h_path = os.path.join(framework_dir, "cores", "arduino", "new.h")
+
+    # Define the path to your custom new.cpp and new.h files
+    custom_new_cpp = os.path.join(env['PROJECT_DIR'], "patch", "new", "src", "new.cpp")
+    custom_new_h = os.path.join(env['PROJECT_DIR'], "patch", "new", "src", "new.h")
+
+    # Check if the custom files exist, and if so, replace the framework's files
+    if os.path.exists(custom_new_cpp) and os.path.exists(custom_new_h):
+      print(f"Replacing {new_cpp_path} with custom new.cpp")
+      shutil.copy(custom_new_cpp, new_cpp_path)
+      print(f"Replacing {new_h_path} with custom new.h")
+      shutil.copy(custom_new_h, new_h_path)
+    else:
+      print("Custom new.cpp and new.h files not found!")
+
+patch_new_lib()
